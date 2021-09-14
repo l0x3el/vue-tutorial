87c03ad4e8cfb33f455b5d932bb3c4e641519ec8
l0x3el(Miho Matsuo) <43780179+l0x3el@users.noreply.github.com>
2021-09-14 23:21:48 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ export default {
   methods: {
     async getEmployees() {
       try {
-        const response = await fetch('https://www.dropbox.com/s/ak6kp908jl730hs/users.json?dl=0&raw=true')
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
         const data = await response.json()
         this.employees = data
       } catch (error) {
@@ -47,7 +47,7 @@ export default {
 
     async addEmployee(employee) {
       try {
-        const response = await fetch('https://www.dropbox.com/s/ak6kp908jl730hs/users.json?dl=0&raw=true', {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users', {
           method: 'POST',
           body: JSON.stringify(employee),
           headers: { "Content-type": "application/json; charset=UTF-8" }
